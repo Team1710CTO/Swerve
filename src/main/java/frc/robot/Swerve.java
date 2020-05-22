84668579a8eb9ef6d35596be4992964690003601
unknown <hudsonhurtig@yahoo.com>
2020-05-22 00:09:56 -0500

    hhrh

---
+++ src/main/java/frc/robot/Swerve.java
@@ -0,0 +1,42 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.GenericHID.Hand;
+import com.kauailabs.navx.frc.AHRS;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.ControlType;
+import com.revrobotics.CANSparkMax.IdleMode;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import java.lang.Math;
+/**
+ * Add your docs here.
+ */
+public class Swerve {
+    public final double L = 23;
+    public final double W = 23;
+    public void drive (double x1, double y1, double x2) {
+        double r = Math.sqrt ((L * L) + (W * W));
+        y1 *= -1;
+    
+        double a = x1 - x2 * (L / r);
+        double b = x1 + x2 * (L / r);
+        double c = y1 - x2 * (W / r);
+        double d = y1 + x2 * (W / r);
+    
+        double backRightSpeed = Math.sqrt ((a * a) + (d * d));
+        double backLeftSpeed = Math.sqrt ((a * a) + (c * c));
+        double frontRightSpeed = Math.sqrt ((b * b) + (d * d));
+        double frontLeftSpeed = Math.sqrt ((b * b) + (c * c));
+    
+        double backRightAngle = Math.atan2 (a, d) / Math.PI;
+        double backLeftAngle = Math.atan2 (a, c) / Math.PI;
+        double frontRightAngle = Math.atan2 (b, d) / Math.PI;
+        double frontLeftAngle = Math.atan2 (b, c) / Math.PI;
+    }
+}
